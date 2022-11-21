4685643ebb933393ed065fb8a09a4ed3b74d4638
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2022-11-21 14:07:49 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ /*   int num1, num2, ans;
+
+int sum(){
+    ans = num1+num2;
+    printf("Sum is : %d", ans);
+    }
+
+int sub(){
+    ans = num1-num2;
+    printf("\nDiferrent is : %d", ans);
+    }
+
+int mul(){
+    ans = num1*num2;
+    printf("\nProduct is : %d", ans);
+    }
+
+int divide(){
+    ans = num1/num2;
+    printf("\nDivide is : %.2d", ans);
+    }
+*/
+
+int main()
+{
+
+/*printf("Enter two numbers : ");
+    scanf("%d %d", &num1, &num2);
+
+    sum(num1, num2, sum);
+    sub(num1, num2, sum);
+    mul(num1, num2, sum);
+    divide(num1, num2, sum);
+    */
+    /*int a=15,b;
+
+    printf();
+*/
+int marks;
+printf("Enter your marks : ");
+scanf("%d", &marks);
+
+if (marks>=75 && marks<=100){
+    printf("A Pass");
+}
+if (marks>=65 && marks<=75){
+    printf("B Pass");
+}
+if (marks>=55 && marks<=65){
+    printf("C Pass");
+}
+if (marks>=45 && marks<=55){
+    printf("S Pass");
+}
+if (marks>=0 && marks<=45){
+    printf("Fail");
+}
+if (marks<=0 && marks>100){
+    printf("Invalid Input");
+}
+
+    return 0;
+}
